7d359bfda379bc9a1a55fcfb2a3ee61d84d35713
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-18 13:23:50 +0900

    #3. app 앱 post_list 뷰 구성하기

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from app.models import Post
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+
+    return render(
+        request,
+        "app/index.html",
+        {
+            "post_list": qs,
+        },
+    )
