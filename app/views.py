b2cf7a81264271bcb52e2aec100b64ee53bc1a44
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-18 14:02:48 +0900

    #3. app 앱 post_detail 뷰 구성

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from app.models import Post
 
@@ -12,5 +12,17 @@ def index(request: HttpRequest) -> HttpResponse:
         "app/index.html",
         {
             "post_list": qs,
+        },
+    )
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    # post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(
+        request,
+        "app/post_detail.html",
+        {
+            "post": post,
         },
     )
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ from app import views as app_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("app/", app_views.index),
+    path("app/<int:pk>/", app_views.post_detail),
 ]
